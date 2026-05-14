c893417d3964a3dfa41ed413bf1dce699027fdb9
Akeem Mudashiru <akeemmudash@gmail.com>
2026-05-14 19:43:15 +0100

    style: prettier reformat of archived useFetchConversion hook

--- archive/currency-selector/utilities/hooks/useFetchConversion.js
+++ archive/currency-selector/utilities/hooks/useFetchConversion.js
@@ -1,26 +1,31 @@
 // src/hooks/useFetchConversion.js
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import axios from 'axios';
-import { setConversionRates } from '../../redux/features/currencySlice';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { setConversionRates } from "../../redux/features/currencySlice";
 
 const useFetchConversion = (amount) => {
   const dispatch = useDispatch();
-  const selectedCurrency = useSelector((state) => state.currency.selectedCurrency);
-  const baseCurrency = 'NGN'; // or your base currency
+  const selectedCurrency = useSelector(
+    (state) => state.currency.selectedCurrency,
+  );
+  const baseCurrency = "NGN"; // or your base currency
   const API_KEY = process.env.EXPO_PUBLIC_EXCHANGERATESAPI_ACCESS_KEY;
 
   useEffect(() => {
     const fetchConversion = async () => {
       try {
-        const response = await axios.get('https://api.exchangeratesapi.io/v1/convert', {
-          params: {
-            access_key: API_KEY,
-            from: baseCurrency,
-            to: selectedCurrency,
-            amount,
+        const response = await axios.get(
+          "https://api.exchangeratesapi.io/v1/convert",
+          {
+            params: {
+              access_key: API_KEY,
+              from: baseCurrency,
+              to: selectedCurrency,
+              amount,
+            },
           },
-        });
+        );
         const conversionRates = response.data;
         dispatch(setConversionRates(conversionRates));
       } catch (error) {
@@ -37,7 +42,7 @@ const useFetchConversion = (amount) => {
           // Something happened in setting up the request that triggered an Error
           // console.error('Error message:', error.message);
         }
-      }      
+      }
     };
 
     if (amount && selectedCurrency) {
